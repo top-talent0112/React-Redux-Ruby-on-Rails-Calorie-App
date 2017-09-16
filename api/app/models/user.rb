97eb7e0c68038d384279b9828a892e8fca980f7f
Dream High <dreamhigh0822@outlook.com>
2017-09-16 18:30:13 +1000

    Api - user role

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :rememberable, :trackable, :recoverable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+
+  enum role: {
+    admin:        "admin",
+    user_manager: "user_manager",
+    regular:      "regular"
+  }
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915175018) do
+ActiveRecord::Schema.define(version: 20170916082434) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
