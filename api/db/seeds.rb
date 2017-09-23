ac0809fde28dad7583de7d2416d497debcefda12
Dream High <dreamhigh0822@outlook.com>
2017-09-23 21:24:30 +1000

    Api - seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -19,3 +19,33 @@ create_user("player2@calories.com", "password", "Luis Suarez", User.roles[:regul
 create_user("player3@calories.com", "password", "Andres Iniesta", User.roles[:regular])
 create_user("player4@calories.com", "password", "Sergio Bousquets", User.roles[:regular])
 create_user("player5@calories.com", "password", "Gerard Pique", User.roles[:regular])
+
+
+def create_meal(user, title, time, calories)
+  meal = Meal.create!(
+    user: user,
+    title: title,
+    time: time,
+    calories: calories
+  )
+end
+
+i = 0
+50.times {
+  create_meal(User.find_by(email: "player1@calories.com"), "Breakfast", (Time.now.utc-i.days).change({hour:7, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player1@calories.com"), "Lunch", (Time.now.utc-i.days).change({hour:13, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player1@calories.com"), "Dinner", (Time.now.utc-i.days).change({hour:19, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player2@calories.com"), "Breakfast", (Time.now.utc-i.days).change({hour:7, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player2@calories.com"), "Lunch", (Time.now.utc-i.days).change({hour:13, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player2@calories.com"), "Dinner", (Time.now.utc-i.days).change({hour:19, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player3@calories.com"), "Breakfast", (Time.now.utc-i.days).change({hour:7, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player3@calories.com"), "Lunch", (Time.now.utc-i.days).change({hour:13, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player3@calories.com"), "Dinner", (Time.now.utc-i.days).change({hour:19, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player4@calories.com"), "Breakfast", (Time.now.utc-i.days).change({hour:7, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player4@calories.com"), "Lunch", (Time.now.utc-i.days).change({hour:13, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player4@calories.com"), "Dinner", (Time.now.utc-i.days).change({hour:19, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player5@calories.com"), "Breakfast", (Time.now.utc-i.days).change({hour:7, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player5@calories.com"), "Lunch", (Time.now.utc-i.days).change({hour:13, min:0, sec:0}), rand(50)+100)
+  create_meal(User.find_by(email: "player5@calories.com"), "Dinner", (Time.now.utc-i.days).change({hour:19, min:0, sec:0}), rand(50)+100)
+  i = i + 1
+}
