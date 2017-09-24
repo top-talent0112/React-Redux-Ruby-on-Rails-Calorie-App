2beaa31fd9ca7f140720c0e7dd83fc2decffb041
Dream High <dreamhigh0822@outlook.com>
2017-09-24 16:24:26 +1000

    Front - Dashboard

--- front/src/containers/dashboard.js
+++ front/src/containers/dashboard.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { Collapse, Grid, Navbar, Nav, NavItem, Row, Col, Label } from "react-bootstrap"
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-      	Dashboard
+        <Row>
+          <Col xs={8} xsOffset={2} className="text-center">
+            <h1>This is Calories Management Application</h1>
+          </Col>
+        </Row>
       </div>
-    );
+    )
   }
 }
 
